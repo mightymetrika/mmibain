2fa9c05ffdf8885676ee02a6de3f443555f70289
Mackson Ncube <macksonncube@gmail.com>
2023-10-23 15:34:27 +0000

    Finish initial draft of mmibain; start on RC game mgmt funcs

--- R/mmibain.R
+++ R/mmibain.R
@@ -1,50 +1,85 @@
+#' mmibain Shiny App
+#'
+#' This function launches a Shiny app that facilitates a user-friendly interface
+#' for setting up and running a Bayesian Informative Hypotheses Evaluation using
+#' the `bain` package.
+#'
+#' @description The user can upload CSV data; choose a model engine (lm, t_test,
+#' lavaan); specify the formula, variables, or model; and provide additional
+#' arguments. Once the model is fitted, the app allows for setting up hypotheses
+#' for evaluation. Upon running the analysis, it displays the results of the
+#' Bayesian Informative Hypotheses Evaluation.
+#'
+#' @details The app's UI consists of a sidebar for user inputs and a main panel
+#' for displaying available variables, model terms, and analysis results. The app
+#' relies on the `bain` package for analysis.
+#'
+#' @section UI Components:
+#' \itemize{
+#'  \item Data upload (CSV format).
+#'  \item Engine selection (lm, t_test, lavaan).
+#'  \item Model input based on chosen engine.
+#'  \item Additional arguments for statistical model function.
+#'  \item Action button to fit the model.
+#'  \item Hypotheses input.
+#'  \item Fraction input for the `bain` fraction parameter.
+#'  \item Option to evaluate hypotheses with respect to standardized regression
+#'  coefficients.
+#'  \item Confidence interval input.
+#'  \item Seed input for reproducibility.
+#'  \item Action button to run the Bayesian Informative Hypotheses Evaluation.
+#' }
+#'
+#' @return This function returns a running instance of the Shiny app.
+#' Interact with the app through the browser or the RStudio Viewer pane.
+#'
+#' @examples
+#' if(interactive()){
+#'   mmibain()
+#' }
+#'
+#' @seealso \code{\link[bain]{bain}}
+#'
+#' @export
+#'
+#' @references Hoijtink, H., Mulder, J., van Lissa, C., & Gu, X. (2019). A
+#' tutorial on testing hypotheses using the Bayes factor. Psychological methods,
+#' 24(5), 539–556. https://doi.org/10.1037/met0000201
 mmibain <- function(){
   # UI
   ui <- shiny::fluidPage(
-    shiny::titlePanel("MMIBain Analysis"),
+    shiny::titlePanel("Set Up Bain Analysis"),
     shiny::sidebarLayout(
       shiny::sidebarPanel(
-        # CSV Input
         shiny::fileInput("datafile", "Choose CSV File",
                          multiple = FALSE,
                          accept = c("text/csv",
                                     "text/comma-separated-values,text/plain",
                                     ".csv")),
-        # Engine selection
         shiny::selectInput("engine", "Choose engine:", choices = c("lm", "t_test", "lavaan")),
         shiny::uiOutput("model_input_ui"),
-        # Additional arguments
         shiny::textInput("additional_args", "Additional Arguments (key=value format)"),
-        # 6. Fit model button
         shiny::actionButton("fit", "Fit Model"),
-        # 7. Displaying the terms for hypotheses
-        # 8. User hypotheses
         shiny::textAreaInput("hypotheses", "Hypotheses", ""),
-        # 9. Fraction input
         shiny::numericInput("fraction", "Fraction", value = 1),
-        # 10. Standardize option
         shiny::checkboxInput("standardize", "Standardize", value = FALSE),
-        shiny::numericInput("seed_value", "Set seed (optional)", value = NULL, min = 1, max = .Machine$integer.max),
-        # New - Run Analysis button
+        shiny::numericInput("ci_value", "Confidence Interval", value = 0.95, min = 0, max = 1),
+        shiny::numericInput("seed_value", "Set seed (optional)", value = NA, min = 1, max = .Machine$integer.max),
         shiny::actionButton("run_analysis", "Run Analysis")
       ),
       shiny::mainPanel(
-        # Variable names display
         shiny::htmlOutput("variables_section"),
         shiny::uiOutput("model_terms_header"),
         shiny::verbatimTextOutput("model_terms"),
-        # 11 & 12. Outputs for model fit and bain analysis
-        shiny::verbatimTextOutput("model_output"),
-        shiny::verbatimTextOutput("bain_output")
+        shiny::uiOutput("bain_results_header"),
+        shiny::verbatimTextOutput("print_bain_output"),
+        shiny::verbatimTextOutput("summary_bain_output")
       )
     )
   )
 
   # Server
   server <- function(input, output, session) {
-
-    # At the beginning of your server function
-    rv <- shiny::reactiveValues(hypotheses = NULL, fraction = 1, standardize = FALSE)
 
     # Reactive: Read the uploaded CSV file
     uploaded_data <- shiny::reactive({
@@ -161,50 +196,50 @@ mmibain <- function(){
       }
     })
 
-    # Observe changes and update reactive values
-    shiny::observe({
-      rv$hypotheses <- input$hypotheses
-      rv$fraction <- input$fraction
-      rv$standardize <- input$standardize
-    })
-
-
     # Logic to run bain analysis on the fitted model
+
+    # Set reactive value to flag when analysis is done
+    bain_analysis_done <- shiny::reactiveVal(FALSE)
+
     shiny::observeEvent(input$run_analysis, {
       shiny::req(model_fitted())
 
       # If seed value is provided, set it
-      if (!is.null(input$seed_value)) {
+      if (!is.na(input$seed_value)) {
         set.seed(input$seed_value)
       }
 
-      # Use the user-provided hypotheses for bain
-      #hypotheses <- input$hypotheses
-
-      #tryCatch({
+      tryCatch({
         # Run bain analysis
-
-        #bain_result <- bain::bain(model, hypothesis = hypotheses, fraction = input$fraction, standardize = input$standardize)
-        #bain_result <- bain::bain(model, hypothesis = "grpGroup1 < grpGroup2 < grpGroup3", fraction = 1, standardize = FALSE)
-        #bain_result <- bain::bain(model, hypothesis = rv$hypotheses, fraction = rv$fraction, standardize = rv$standardize)
-        bain_result <- bain::bain(model, hypothesis = rv$hypotheses(), fraction = rv$fraction(), standardize = rv$standardize())
-
+        bain_result <- do.call(bain::bain, args = list(x = model,
+                                                       hypothesis = input$hypotheses,
+                                                       fraction = input$fraction,
+                                                       standardize = input$standardize))
 
         # Set outputs for the model and bain analysis
-        output$model_output <- shiny::renderPrint({ print(bain_result) })
-        output$bain_output <- shiny::renderPrint({ summary(bain_result, ci = 0.95) })
+        output$print_bain_output <- shiny::renderPrint({ print(bain_result) })
+        output$summary_bain_output <- shiny::renderPrint({ summary(bain_result, ci = input$ci_value) })
 
-      # }, error = function(e) {
-      #   shiny::showNotification(
-      #     paste("Error:", e$message),
-      #     type = "error",
-      #     duration = NULL
-      #   )
-      # })
+        # Flag that bain analysis is done
+        bain_analysis_done(TRUE)
+
+        }, error = function(e) {
+          shiny::showNotification(
+            paste("Error:", e$message),
+            type = "error",
+            duration = NULL
+            )
+
+          # Flag that bain analysis is not done
+          bain_analysis_done(FALSE)
+          })
     })
 
-
+    output$bain_results_header <- shiny::renderUI({
+      if(bain_analysis_done()) {
+        shiny::tags$h2("Bayesian Informative Hypotheses Evaluation Results")
+      }
+    })
   }
-
   shiny::shinyApp(ui = ui, server = server)
 }
