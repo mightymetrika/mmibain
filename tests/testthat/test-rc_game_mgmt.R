02c3d3f16451ac7692cdc2cecaa3d99cf77ac58b
Mackson Ncube <macksonncube@gmail.com>
2023-10-25 15:30:59 +0000

    add process_replication_study

--- tests/testthat/test-rc_game_mgmt.R
+++ tests/testthat/test-rc_game_mgmt.R
@@ -1,7 +1,39 @@
-# test_that("basic work flow works", {
-#   result <- deal_cards_to_rc_grid(n = 3)
-#   study_data <- generate_study_data(result, sample_size = 100)
-#   study_results <- process_original_study(study_data)
-#   bain_res <- bain::bain(study_results$fit, hypothesis = study_results$hypothesis)
-#   expect_s3_class(bain_res, "bain")
-# })
+test_that("basic original study work flow works", {
+
+  set.seed(100)
+
+  # Run basic work flow
+  result <- deal_cards_to_rc_grid(n = 3)
+  study_data <- generate_study_data(result, sample_size = 100)
+  study_results <- process_original_study(study_data)
+  #bain_res <- bain::bain(study_results$fit, hypothesis = study_results$hypothesis)
+
+  # Test expectations
+  expect_equal(length(result), 2)
+  expect_s3_class(result$updated_deck, "UpDeck")
+  expect_s3_class(study_data, "data.frame")
+  expect_s3_class(study_results$fit, "aov")
+  expect_s3_class(study_results$fit, "lm")
+})
+
+test_that("basic game work flow works",{
+
+  set.seed(250)
+
+  # Run basic work flow
+
+  ## Original study
+  os_deck <- deal_cards_to_rc_grid(n = 3)
+  original_study_data <- generate_study_data(os_deck, sample_size = 100)
+  original_study_results <- process_original_study(original_study_data)
+
+  ## Replication study
+  rs_deck <- deal_cards_to_rc_grid(n = 3)
+  replication_data <- generate_study_data(rs_deck, sample_size = 100)
+  replication_results <- process_replication_study(replication_data,
+                                                   original_study_results)
+
+  # Test expectations
+  expect_s3_class(replication_results, "bain")
+
+})
